f251fa954dd2037121fcaa576d9929022265bb53
circl <circl.lastname@gmail.com>
2025-10-26 20:18:25 +0100

    Speed it up if picture already handled

--- generate.js
+++ generate.js
@@ -3,16 +3,21 @@
 const { chdir } = require("node:process")
 const { execFileSync } = require("node:child_process");
 const { createHash } = require("node:crypto");
-const { existsSync, writeFileSync } = require("node:fs");
+const { existsSync, writeFileSync, readFileSync } = require("node:fs");
 
 console.log("Running exiftool...");
 
 chdir(__dirname);
 const metadata = JSON.parse(execFileSync("exiftool", [ "-r", "-j", "music" ], { encoding: "utf-8" }));
 
+let oldOutput = {};
 let output = {};
 let processedFiles = 0;
 let importedPictures = 0;
+
+if (existsSync("info.json")) {
+  oldOutput = JSON.parse(readFileSync("info.json", "utf-8"));
+}
 
 for (let file of metadata) {
   if (!file.MIMEType.startsWith("audio/")) {
@@ -24,23 +29,6 @@ for (let file of metadata) {
   const album = file.Album ? file.Album : file.FileName;
   const title = file.Title ? file.Title : file.FileName;
   
-  const picture = file.Picture ? execFileSync("exiftool", [ "-b", "-picture", file.SourceFile ]) : null;
-  let pictureHash = "";
-  
-  if (picture) {
-    const hash = createHash("md5");
-    hash.update(picture);
-    pictureHash = hash.digest("hex");
-  }
-  
-  if (pictureHash !== "") {
-    if (!existsSync(`pictures/${pictureHash}`)) {
-      writeFileSync(`pictures/${pictureHash}`, picture);
-      console.log(`Imported picture ${pictureHash}`);
-      importedPictures++;
-    }
-  }
-  
   if (!output[artist]) {
     output[artist] = {};
   }
@@ -51,10 +39,31 @@ for (let file of metadata) {
   
   const trackNumber = file.TrackNumber ? parseInt(file.TrackNumber)-1 : output[artist][album].length;
   
+  let picturePath = "";
+  
+  // If this track appears in the old info.json, skip handling the picture
+  if (oldOutput[artist]?.[album]?.[trackNumber]?.title === title) {
+    picturePath = oldOutput[artist][album][trackNumber].picture;
+  } else {
+    let picture = file.Picture ? execFileSync("exiftool", [ "-b", "-picture", file.SourceFile ]) : null;
+    
+    if (picture) {
+      const hash = createHash("md5");
+      hash.update(picture);
+      picturePath = `pictures/${hash.digest("hex")}`;
+      
+      if (!existsSync(picturePath)) {
+        writeFileSync(picturePath, picture);
+        console.log(`Imported picture ${picturePath}`);
+        importedPictures++;
+      }
+    }
+  }
+  
   output[artist][album][trackNumber] = {
     title: title,
-    file: `${file.SourceFile}`,
-    picture: pictureHash !== "" ? `pictures/${pictureHash}` : "pictures/no-picture.svg",
+    file: file.SourceFile,
+    picture: picturePath !== "" ? picturePath : "pictures/no-picture.svg",
   };
   
   processedFiles++;
